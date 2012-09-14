bade72a99c236bea57aeb6389a2c1170035aea6b
coinor <coinor@ubuntu.(none)>
2012-09-14 09:17:36 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaskmanagerFinal::Application.config.secret_token = '43ab647e9e3e1d9898d7a187008c2a9c4a04cb68c374293e9de53e74feed42b99264d605aae8b8e902ed53ec427c950fd4c4fabce0e4573a8c95811eaf03fcf4'

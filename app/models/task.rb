02bc8cb24f99ad4ce1b93abf7abedf03fd6b944a
coinor <coinor@ubuntu.(none)>
2012-09-15 01:21:34 -0700

    Test

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ActiveRecord::Base
   attr_accessible :deadline, :description, :name, :priority
-  
-  belongs_to :status
   has_many :comments
+  belongs_to :status
+  belongs_to :user
+  belongs_to :project
 end

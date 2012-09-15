dc54e12019f1b742bb461d6d2e67b479e58a6c85
chris <chris@192.168.1.199>
2012-09-15 23:54:36 +0200

    Sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+    
+  end
+
+  def create
+     user = User.find_by_email(params[:email])
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect_to :controller => "tasks", :action => "administration"
+  else
+    flash.now.alert = "Invalid email or password"
+    render "new"
+  end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    self.session_checked = false
+    redirect_to :controller => "sessions", :action => "create"
+  end
+end

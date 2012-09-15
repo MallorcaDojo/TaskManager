c0a1c66cf8ee60e8199406fffa66ea3d1419daf9
MallorcaDojo <dojo12345>
2012-09-15 11:53:06 +0200

    erweitert

--- app/models/priority.rb
+++ app/models/priority.rb
@@ -1,5 +1,6 @@
 class Priority < ActiveEnum::Base
-  value :id => 1, :name => 'Low'
+  value :id => 1, :name => 'High'
   value :id => 2, :name => 'Middle'
-  value :id => 3, :name => 'High'
+  value :id => 3, :name => 'Low'
+  value :id => 4, :name => 'Lowest'
 end

f1964aeafc978482a2162dac248985e83fba2c43
cfinkelstein <cfinkelstein@NB058.coinor.local>
2012-09-15 17:12:18 +0200

    [finkelsc] TestFile DragDrop

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,21 @@
 class TasksController < ApplicationController
   
   
+  def test
+    @tasks = Task.all
+    
+    if @tasks.size == 0
+      Task.new(:name=>"Task0",:description=>"Desc0").save
+      Task.new(:name=>"Task1",:description=>"Desc1").save
+      Task.new(:name=>"Task2",:description=>"Desc2").save
+      Task.new(:name=>"Task3",:description=>"Desc3").save
+      Task.new(:name=>"Task4",:description=>"Desc4").save
+      Task.new(:name=>"Task5",:description=>"Desc5").save
+   
+      @tasks = Task.all     
+    end
+  end
+  
   def administration
     @tasks = Task.all
     render action: "index"

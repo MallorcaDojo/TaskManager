a018c44af8eae89f8551da2665a8858dcc5722d6
MallorcaDojo <dojo12345>
2012-09-15 11:52:34 +0200

    Unit Test

--- test/unit/task_test.rb
+++ test/unit/task_test.rb
@@ -4,4 +4,37 @@ class TaskTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def test_tasks
+    task1 = Task.new(
+      :deadline => "2012-09-30", 
+      :description => "Erstellung der Masken",
+      :name => "GUI-Erstellung", 
+      :priority => 2
+     )
+     
+    task2 = Task.new(
+      :deadline => "2012-09-29", 
+      :description => "Implementierung der Logik im Controller",
+      :name => "Controller", 
+      :priority => 2
+     )
+     
+    task3 = Task.new(
+      :deadline => "2012-09-28", 
+      :description => "Dokumentation von Model, View, Controller",
+      :name => "Doku", 
+      :priority => 10
+     )
+     
+    task4 = Task.new(
+      :deadline => "2012-09-27", 
+      :description => "Darstellung der Abhaengigkeiten im Model",
+      :name => "Model", 
+      :priority => 1
+     )
+     assert task1.save, "Did not save task1"
+     assert task2.save, "Did not save task2"
+     assert task3.save, "Did not save task3"
+     assert task4.save, "Did not save task4"
+  end   
 end

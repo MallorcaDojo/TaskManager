2b9357f6384d8fe1a251ba5c130231455d502edb
mheck <mheck@NB081-COINOR.coinor.local>
2012-09-15 16:29:23 +0200

    Added multiple validations to model, added default priority to task.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,16 @@
 class Task < ActiveRecord::Base
   attr_accessible :deadline, :description, :name, :priority
+  
   has_many :comments
   belongs_to :status
   belongs_to :user
   belongs_to :project
+  
   enumerate :priority, :with => Priority
+  
+  before_save :set_default
+  
+  def set_default
+    self.priority = 4
+  end
 end

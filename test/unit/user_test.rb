26e13839fa2860a4dfb532b1ce9b974d025c946c
cfinkelstein <cfinkelstein@NB058.coinor.local>
2012-09-15 12:04:00 +0200

    [finkelsc] Testfaelle -> User

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "createUser" do
+     user = User.new
+    user.firstname = "Markus"  
+    user.lastname = "Koscheierle"
+    user.birthday = Date.new(1980,6,8)  
+    assert user.save
+   end
+   
+   test "createUserAndTask" do
+     
+    user = User.new
+    user.firstname = "Markus"  
+    user.lastname = "Koscheierle"
+    user.birthday = Date.new(1980,6,8)
+    
+    task = Task.new
+    user.tasks = Array.new
+    user.tasks.push task
+    task.name = "MyTask"
+    task.priority = 1
+    task.description = "MyDesc"
+    task.deadline = Date.new(2013,6,8)
+    
+      
+    assert user.save
+   end
+   
+   test "createUserAndProject" do
+    user = User.new
+    user.firstname = "Markus"  
+    user.lastname = "Koscheierle"
+    user.birthday = Date.new(1980,6,8)
+    
+    project = Project.new
+    project.name = "MyTestProject"
+    
+    user.projects = Array.new
+    user.projects.push project
+    
+    assert user.save
+   end
 end

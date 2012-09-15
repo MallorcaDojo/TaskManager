3f7c94c9ace68a65cfe3266b58c08e36aa2c1fd2
mheck <mheck@NB081-COINOR.coinor.local>
2012-09-15 12:33:07 +0200

    Even more project tests.

--- test/unit/project_test.rb
+++ test/unit/project_test.rb
@@ -3,7 +3,26 @@ require 'test_helper'
 class ProjectTest < ActiveSupport::TestCase
    test "createProject" do
     project = Project.new
-    project.name = "TestProjekt"
+    project.name = "Testproject"
     assert project.save
+   end
+   
+   test "createProjectAndUser" do
+     project = Project.new
+     project.name = "Testproject with user"
+     
+     user = User.new
+     user.firstname = "Susi"  
+     user.lastname = "Sorglos"
+     user.birthday = Date.new(1988,8,8)
+     assert user.save
+     
+     project.users = Array.new
+     project.users.push user
+     
+     user.projects = Array.new
+     user.projects.push project
+     
+     assert project.save
    end
 end

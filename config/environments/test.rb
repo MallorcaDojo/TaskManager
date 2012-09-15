35d923da3e9bfe1283cb5f3b51627a069870ccc9
cfinkelstein <cfinkelstein@NB058.coinor.local>
2012-09-15 18:03:02 +0200

    [finkelsc] LogTest SQL-Statements

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,7 @@
 TaskmanagerFinal::Application.configure do
+
+  ActiveRecord::Base.logger = Logger.new(STDOUT)
+
   # Settings specified here will take precedence over those in config/application.rb
 
   # The test environment is used exclusively to run your application's
